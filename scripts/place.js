774c84bcfeeed8f5e06136931c5bdf6e565dd876
Camille Hargis <camillehargis@studentbody.byu.edu>
2025-08-02 13:10:57 -0600

    added missing items

--- scripts/place.js
+++ scripts/place.js
@@ -9,3 +9,15 @@ year.innerHTML = today.getFullYear();
 
 let text = document.lastModified;
 document.getElementById("lastModified").innerHTML = text;
+
+// wind chill factor
+function windChillFactor(temperature, windSpeed) {
+    if (temperature <= 50 && windSpeed > 3) {
+        return 35.74 + (0.6215 * temperature) - (35.75 * Math.pow(windSpeed, 0.16)) + (0.4275 * temperature * Math.pow(windSpeed, 0.16));
+    }
+    else {
+        return "N/A";
+    }
+}
+
+document.querySelector("#wind-chill").innerHTML = windChillFactor(84, 18);
